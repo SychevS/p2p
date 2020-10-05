faf9170aec6e0fbb61d446d0a7d1d22a29a6e0ae
Sergey Sychev <sychev_sergey@inbox.ru>
2020-10-05 14:33:41 +0300

    some comments for interface added

--- include/p2p_network.h
+++ include/p2p_network.h
@@ -18,8 +18,11 @@
 
 namespace net {
 
+/// 256 bits identifier with aithmetics support,
+/// you can implement your own, if neccessary.
 using NodeId = arith_uint256;
 using FragmentId = NodeId;
+
 using ByteVector = std::vector<uint8_t>;
 
 /// id + ip + port
@@ -40,19 +43,26 @@ class Manager {
   Manager(const Manager&) = delete;
   Manager& operator=(const Manager&) = delete;
 
+  /// Begins Manager's work in separate thread and return.
   void Start();
 
+  /// Communication with other nodes in the network.
   void SendDirect(const NodeId& to, ByteVector&& msg);
   void SendBroadcast(ByteVector&& msg);
   void SendBroadcastIfNoConnection(const NodeId& to, ByteVector&& msg);
 
+  /// Try to connect to nodes in the passed list.
   void AddKnownNodes(const std::vector<NodeEntry>&);
+
+  /// Get content of routing table.
   void GetKnownNodes(std::vector<NodeEntry>&);
 
+  /// Censorship block.
   void Ban(const NodeId&);
   void Unban(const NodeId&);
   void ClearBanList();
 
+  /// Store/load some data distributed through the network.
   std::vector<FragmentId> StoreValue(ByteVector&& value);
   void FindFragment(const FragmentId&);
 
@@ -61,15 +71,23 @@ class Manager {
   std::unique_ptr<Impl> pimpl_;
 };
 
+/// This interface must be thread safe.
 class EventHandler {
  public:
+  /// Called each time when new message received.
   virtual void OnMessageReceived(const NodeId& from, ByteVector&& message) = 0;
+
+  /// Called on new node discovery.
   virtual void OnNodeDiscovered(const NodeId&) = 0;
+
+  /// Called when node removed from routing table.
   virtual void OnNodeRemoved(const NodeId&) = 0;
 
+  /// One of these two methods is a result of Manager.FindFragment(id)
   virtual void OnFragmentFound(const FragmentId&, ByteVector&& value) = 0;
   virtual void OnFragmentNotFound(const FragmentId& id) = 0;
 
+  /// Called to split value of Manager.StoreValue(value) into smaller fragments.
   virtual FragmentId GetFragmentId(const ByteVector& fragment) = 0;
 };
 
